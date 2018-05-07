dde86b10055fd055f3f7168587d1bfc0dcb6d66b
Ryan Smith <ryan.smith@procore.com>
2018-05-07 14:31:23 -0700

    handle multiple filter inputs

--- lib/brita/collection_handler.rb
+++ lib/brita/collection_handler.rb
@@ -6,11 +6,21 @@ module Brita
 
     def call(collection, value, _params, _scope_params)
       if collection.is_a?(Array)
+        select_from_collection(collection, value)
+      else
+        collection.where(@param.internal_name => value)
+      end
+    end
+
+    def select_from_collection(collection, value)
+      if value.is_a?(Array)
+        collection.select do |item|
+          value.include?(item.with_indifferent_access[@param.internal_name])
+        end
+      else
         collection.select do |item|
           item.with_indifferent_access[@param.internal_name] == value
         end
-      else
-        collection.where(@param.internal_name => value)
       end
     end
   end
