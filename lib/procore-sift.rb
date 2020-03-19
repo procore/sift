07e01181ba20fc7d3ce0469e8c6e103359d8cf69
Nate Baer <njbbaer@gmail.com>
2020-03-19 15:20:15 -0700

    Fix broken code

--- lib/procore-sift.rb
+++ lib/procore-sift.rb
@@ -46,14 +46,10 @@ module Sift
   end
 
   def filters
-    self.class.ancestors.flat_map do |klass|
-      klass.try(:filters)
-    end.compact.uniq do |f|
-      [f.param, f.class]
-    self.class.ancestors.
-      flat_map { |klass| klass.try(:filters) }.
-      compact.
-      uniq { |f| [f.param, f.class] }
+    self.class.ancestors
+      .flat_map { |klass| klass.try(:filters) }
+      .compact
+      .uniq { |f| [f.param, f.class] }
   end
 
   def sorts_exist?
